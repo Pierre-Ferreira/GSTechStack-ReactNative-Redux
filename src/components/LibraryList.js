bc0fe9109a351397f8d2bcc153001f21012bbe50
Pierre Ferreira <pierre@tektite.biz>
2017-02-07 13:14:15 +0200

    Update4

---
+++ src/components/LibraryList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+
+class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow() {
+    
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
+
+const MapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(MapStateToProps)(LibraryList);
